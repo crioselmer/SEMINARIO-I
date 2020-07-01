5fb6e8f89487fd1515d301bd1ed68e9d647325e0
Elmer Cieza Rios <rios_121@hotmail.com>
2020-06-30 19:23:37 -0500

    PROYECTO WEB

--- Crud_4Capas_Grupo3/CapaPresentacion/FrmCasos.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class FrmCasos
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        FrmCliente.Show()
-        Timer1.Enabled = False
-        Me.Hide()
-    End Sub
-End Class
--- Crud_4Capas_Grupo3/CapaPresentacion/FrmPresentacion.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FrmPresentacion
-    Private Sub BtnIngresar_Click_1(sender As Object, e As EventArgs) Handles BtnIngresar.Click
-        FrmCasos.Show()
-        Me.Hide()
-    End Sub
-End Class
